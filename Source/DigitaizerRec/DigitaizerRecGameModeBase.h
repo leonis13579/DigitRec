2d8f3357135dd7b6d8b413c6c4e2f27b61fbd351
Leonis <leonis13579@gmail.com>
2019-10-22 12:06:46 +0300

    Project init

---
+++ Source/DigitaizerRec/DigitaizerRecGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DigitaizerRecGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIGITAIZERREC_API ADigitaizerRecGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
